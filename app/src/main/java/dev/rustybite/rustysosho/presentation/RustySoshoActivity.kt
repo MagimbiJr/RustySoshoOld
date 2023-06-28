9a3b71f26da9680d1456d30888adc39b62ac0449
tana <charleatanasi@gmail.com>
2023-06-28 21:49:23 +0300

    set up navigation and typography

--- app/src/main/java/dev/rustybite/rustysosho/presentation/RustySoshoActivity.kt
+++ app/src/main/java/dev/rustybite/rustysosho/presentation/RustySoshoActivity.kt
@@ -7,19 +7,22 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
+import dev.rustybite.rustysosho.presentation.navigation.RustySoshoNavHost
 import dev.rustybite.rustysosho.presentation.ui.theme.RustySoshoTheme
 
 class RustySoshoActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val navHostController = rememberNavController()
             RustySoshoTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    RustySoshoNavHost(navHostController = navHostController)
                 }
             }
         }
--- app/src/main/java/dev/rustybite/rustysosho/presentation/ui/theme/Type.kt
+++ app/src/main/java/dev/rustybite/rustysosho/presentation/ui/theme/Type.kt
@@ -2,14 +2,23 @@ package dev.rustybite.rustysosho.presentation.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import dev.rustybite.rustysosho.R
 
 // Set of Material typography styles to start with
+
+private val MontserratBold = FontFamily(Font(R.font.montserrat_bold))
+private val MontserratMedium = FontFamily(Font(R.font.montserrat_medium))
+private val MontserratRegular = FontFamily(Font(R.font.montserrat_regular))
+private val MontserratLight = FontFamily(Font(R.font.montserrat_light))
+
+
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = MontserratLight,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
